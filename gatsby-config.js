6b4f179e5b4f7db83579f1a738b66d0be76c08bb
Allan Legemaate <alegemaate@gmail.com>
2021-02-01 20:44:28 -0500

    Add manifest file (#11)

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: "Ads Games",
-    titleTemplate: "%s - Ads Games",
-    description: "Frontend for A.D.S. Games.",
-    keywords: "gatsbyjs, gatsby, javascript",
+    titleTemplate: "%s - A.D.S. Games",
+    description: "A.D.S. Games",
+    keywords: "games, adsgames",
     siteUrl: "https://adsgames.net",
     author: {
       name: "Allan Legemaate",
@@ -12,6 +12,20 @@ module.exports = {
     },
   },
   plugins: [
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        name: "A.D.S. Games",
+        short_name: "A.D.S. Games",
+        start_url: "/",
+        background_color: "#111111",
+        theme_color: "#E43F00",
+        display: "standalone",
+        icon: "src/images/logos/logo.svg",
+        lang: "en",
+        description: "A.D.S. Games website",
+      },
+    },
     {
       resolve: "gatsby-source-filesystem",
       options: {
